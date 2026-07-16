5697ef156570bc9ad777524a9dbc4701801839f6
Md Abdul Aziz <110674887+A7E28@users.noreply.github.com>
2026-07-16 20:48:08 +0600

    fix(xml): target stable channel in repository parser
    - Update XML extraction logic to explicitly look for '<channelRef ref=" channel-0\/>'.

--- src/utils/parse_xml.zig
+++ src/utils/parse_xml.zig
@@ -10,23 +10,34 @@ pub const XmlUtils = struct {
     }
 
     pub fn extractVersionFromXml(self: Self, xml_content: []const u8) !?[]u8 {
-        const package_start = std.mem.indexOf(u8, xml_content, "path=\"platform-tools\"") orelse return null;
+        var search_pos: usize = 0;
+        while (search_pos < xml_content.len) {
+            const package_attr = "path=\"platform-tools\"";
+            const package_start = std.mem.indexOf(u8, xml_content[search_pos..], package_attr) orelse return null;
+            const absolute_start = search_pos + package_start;
 
-        const block_start = std.mem.lastIndexOfScalar(u8, xml_content[0..package_start], '<') orelse return null;
-        const block_end = std.mem.indexOf(u8, xml_content[package_start..], "</remotePackage>") orelse return null;
+            const block_start = std.mem.lastIndexOfScalar(u8, xml_content[0..absolute_start], '<') orelse return null;
+            const block_end_rel = std.mem.indexOf(u8, xml_content[absolute_start..], "</remotePackage>") orelse return null;
+            const block_end = absolute_start + block_end_rel + "</remotePackage>".len;
 
-        const package_block = xml_content[block_start .. package_start + block_end];
+            const package_block = xml_content[block_start..block_end];
 
-        const revision_start = std.mem.indexOf(u8, package_block, "<revision>") orelse return null;
-        const revision_end = std.mem.indexOf(u8, package_block[revision_start..], "</revision>") orelse return null;
+            if (std.mem.indexOf(u8, package_block, "<channelRef ref=\"channel-0\"/>") != null) {
+                const revision_start = std.mem.indexOf(u8, package_block, "<revision>") orelse return null;
+                const revision_end = std.mem.indexOf(u8, package_block[revision_start..], "</revision>") orelse return null;
 
-        const revision_block = package_block[revision_start .. revision_start + revision_end];
+                const revision_block = package_block[revision_start .. revision_start + revision_end];
 
-        const major = self.extractValueBetweenTags(revision_block, "<major>", "</major>") orelse return null;
-        const minor = self.extractValueBetweenTags(revision_block, "<minor>", "</minor>") orelse return null;
-        const micro = self.extractValueBetweenTags(revision_block, "<micro>", "</micro>") orelse return null;
+                const major = self.extractValueBetweenTags(revision_block, "<major>", "</major>") orelse return null;
+                const minor = self.extractValueBetweenTags(revision_block, "<minor>", "</minor>") orelse return null;
+                const micro = self.extractValueBetweenTags(revision_block, "<micro>", "</micro>") orelse return null;
 
-        return try std.fmt.allocPrint(self.allocator, "{s}.{s}.{s}", .{ major, minor, micro });
+                return try std.fmt.allocPrint(self.allocator, "{s}.{s}.{s}", .{ major, minor, micro });
+            }
+
+            search_pos = block_end;
+        }
+        return null;
     }
 
     fn extractValueBetweenTags(self: Self, content: []const u8, start_tag: []const u8, end_tag: []const u8) ?[]const u8 {
